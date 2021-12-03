3e660c0a81e037935333a153e4aa054925ab9543
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-12-03 17:40:09 +0900

    [update] Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="home">
-    <h1>Hello</h1>
+    <Login v-if="!authorized" :redirectURI="redirectURI"></Login>
+    <GetTracksAndInformation :rendered="rendered" :routeParams="routeParams"></GetTracksAndInformation>
   </div>
 </template>
 
 <script>
+import Login from '@/components/Login.vue'
+import GetTracksAndInformation from '@/components/GetTracksAndInformation.vue'
 
 export default {
   name: 'App',
   components: {
-  }
+    Login,
+    GetTracksAndInformation
+  },
+  data: function () {
+    return {
+      redirectURI: 'http://localhost:8080/SpotiList/', // 開発用
+      // redirectURI: 'https://tai5863.github.io/SpotiList/', // 本番用
+      authorized: false,
+      rendered: false
+    }
+  },
+  props: {
+    routeParams: Object
+  },
+  created: function () {
+    if (this.$route.hash) {
+      this.$router.push(this.$route.fullPath.replace('#', '?'));
+      this.authorized = true;
+      this.rendered = true;
+    }
+  },
 }
 </script>
 
