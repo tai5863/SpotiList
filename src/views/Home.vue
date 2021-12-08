84aa8681367086e069ae3f8434725dc975a3f43a
kota-nakayama329 <n2001kouta@i.softbank.jp>
2021-12-08 14:37:13 +0900

    タイトルと説明文

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,46 @@
 <template>
   <div id="home">
+    <h1>Spotilist</h1>
+    <p>
+      SpotiListは、Spotifyのお気に入りの曲を分析し、類似した曲のプレイリストを作成するアプリです。
+    </p>
     <Login v-if="!authorized" :redirectURI="redirectURI"></Login>
-    <GetTracksAndInformation :rendered="rendered" :routeParams="routeParams"></GetTracksAndInformation>
+    <GetTracksAndInformation
+      :rendered="rendered"
+      :routeParams="routeParams"
+    ></GetTracksAndInformation>
   </div>
 </template>
 
 <script>
-import Login from '@/components/Login.vue'
-import GetTracksAndInformation from '@/components/GetTracksAndInformation.vue'
+import Login from "@/components/Login.vue";
+import GetTracksAndInformation from "@/components/GetTracksAndInformation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Login,
-    GetTracksAndInformation
+    GetTracksAndInformation,
   },
   data: function () {
     return {
-      redirectURI: 'http://localhost:8080/SpotiList/', // 開発用
+      redirectURI: "http://localhost:8080/SpotiList/", // 開発用
       // redirectURI: 'https://tai5863.github.io/SpotiList/', // 本番用
       authorized: false,
-      rendered: false
-    }
+      rendered: false,
+    };
   },
   props: {
-    routeParams: Object
+    routeParams: Object,
   },
   created: function () {
     if (this.$route.hash) {
-      this.$router.push(this.$route.fullPath.replace('#', '?'));
+      this.$router.push(this.$route.fullPath.replace("#", "?"));
       this.authorized = true;
       this.rendered = true;
     }
   },
-}
+};
 </script>
 
 <style>
@@ -44,5 +51,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+  font-size: 80px;
+}
+p {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+  font-size: 20px;
 }
 </style>
