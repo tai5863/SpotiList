0fd6edb108ec9c808c73b2da8517805f35b58b2a
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-12-10 17:48:02 +0900

    [update] get favorite tracks

--- src/components/GetTracksInformation.vue
+++ src/components/GetTracksInformation.vue
@@ -14,33 +14,106 @@ export default {
   props: {
     routeParams: Object,
   },
-  methods: {
-    init() {
-      // this.getUsersID();
-      console.log(this.routeParams); 
-    },
-    async getUsersID() {
-      const axios = require("axios");
-      let endpoint = "https://api.spotify.com/v1/me/";
-      let data = {
+  data() {
+    return {
+      userId: "",
+      axios: require("axios"),
+      limit: 50,
+      total: 0,
+      offset: 0,
+      favorites: [],
+    };
+  },
+  computed: {
+    data() {
+      return {
         headers: {
           Authorization:
             this.routeParams.token_type + " " + this.routeParams.access_token,
         },
         data: {},
-      };
+      }
+    }
+  },
+  methods: {
+    async init() {
+      await this.getUserId();
+      await this.getTotalNumber();
+      await this.getTracks();
+      console.log("finished");
+    },
+    getUserId() {
+      return new Promise((resolve, reject) => {
+        let endpoint = "https://api.spotify.com/v1/me/";
 
-      axios
-      .get(endpoint, data)
-      .then((res) => {
-        this.userID = res.data.id;
-      })
-      .catch((err) => {
-        console.error(err);
-        alert("エラーが発生しました");
-        window.location.href = "";
+        this.axios
+          .get(endpoint, this.data)
+          .then((res) => {
+            this.userId = res.data.id;
+            console.log("get userId");
+            resolve("get userId");
+          })
+          .catch((err) => {
+            alert("エラーが発生しました");
+            window.location.href = "";
+            reject(err);
+          });
       });
     },
+    getTotalNumber() {
+      return new Promise((resolve, reject) => {
+        let endpoint_first =
+          "https://api.spotify.com/v1/me/tracks?limit=" +
+          String(this.limit) +
+          "&market=JP";
+        this.axios
+          .get(endpoint_first, this.data)
+          .then((res_first) => {
+            this.favorites = res_first.data.items;
+            this.total = res_first.data.total;
+            console.log('total: ' + String(this.total));
+            resolve("get favorites");
+          })
+          .catch((err) => {
+            alert("エラーが発生しました");
+            window.location.href = "";
+            reject(err);
+          });
+      });
+    },
+    getTracks() {
+      return new Promise((resolve, reject) => {
+        if (this.total > this.limit) {
+          let promises = [];
+          let loop_num = Math.floor(this.total / this.limit);
+
+          for (let i = 0; i < loop_num; i++) {
+            this.offset += this.limit;
+            let endpoint_next =
+              "https://api.spotify.com/v1/me/tracks?offset=" +
+              String(this.offset) +
+              "&limit=50&market=JP";
+            promises.push(this.axios.get(endpoint_next, this.data));
+          }
+
+          this.axios
+            .all(promises)
+            .then((res_next) => {
+              res_next.forEach((element) => {
+                this.favorites = this.favorites.concat(element.data.items);
+              });
+              console.log(this.favorites);
+              resolve("get tracks");
+            })
+            .catch((err) => {
+              console.error(err);
+              alert("エラーが発生しました");
+              window.location.href = "";
+              reject(err);
+            });
+        } 
+      });
+    }
   },
 };
 </script>
