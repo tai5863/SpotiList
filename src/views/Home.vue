a4e5c5bc92f210aa108f331f1163c6a4378aa61d
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-12-23 22:58:34 +0900

    Update: apply three-css3drenderer to my visual

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,9 +24,6 @@ export default {
 
 <style scoped>
 #home {
-  font-family: 'Abel', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
